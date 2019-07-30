2e5e95c966bfbd11a6244543794a1280431038ad
Adrian Gjerstad <adgjerstad@gmail.com>
2019-07-30 16:20:40 +0000

    Created delay

--- examples/version/index.js
+++ examples/version/index.js
@@ -1,11 +1,11 @@
 function start() {
   mn.devtools.verbose(mn.sound.speakers.constructor.name);
 
-  let osc = new mn.sound.Oscillator(new mn.sound.Note("C4"), "sawtooth");
+  let osc = new mn.sound.Oscillator(new mn.sound.Note("C4"), "sine");
   let gain = new mn.sound.GainFX(4);
-  let pan = new mn.sound.PanFX(mn.sound.PAN.LEFT);
+  let delay = new mn.sound.DelayFX(5.0);
   osc.appendFilter(gain);
-  osc.appendFilter(pan);
+  osc.appendFilter(delay);
   window.onclick = function() {
     osc.start();
     osc.rampFrequency(new mn.sound.Note("C5").frequency, 5);
@@ -22,3 +22,4 @@ mn.loadPackage("mn.sound.note", {path: "../../src/"});
 mn.loadPackage("mn.sound.oscillator", {path: "../../src/"});
 mn.loadPackage("mn.sound.fx.gain", {path: "../../src/"});
 mn.loadPackage("mn.sound.fx.pan", {path: "../../src/"});
+mn.loadPackage("mn.sound.fx.delay", {path: "../../src/"});
